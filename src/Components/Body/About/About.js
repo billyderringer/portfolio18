153a2cdd91bd2e677b87b161c00bab2c9ea0b8ba
Billy Derringer <billyderringer7@gmail.com>
2019-02-03 20:16:03 -0600

    added reveal to nav links

--- src/Components/Body/About/About.js
+++ src/Components/Body/About/About.js
@@ -1,20 +1,26 @@
 import React, { Component } from 'react'
+import AnchorLink from 'react-anchor-link-smooth-scroll'
 import './About.css'
 
 class About extends Component{
     render(){
+      let skills = "#container-skills"
         return(
             <section id="container-about"
                      className="center-all-flex flex-column">
-                <h2 id="about-title"
-                    className="section-title">
-                    <span id="focus">WELCOME!</span>
-                </h2>
-                <p>
-                    In short, I am a developer whose goal is to find the
-                  best solution for any given problem. Because of this, I am constantly learning new technologies and methods
-                  for improving my workflow.<br className="no-show-tab"/> Check out some of my work below.
-                </p>
+              <h2 id="about-title"
+                  className="section-title">
+                  <span id="focus">WELCOME!</span>
+              </h2>
+              <p>
+                  In short, I am a developer whose goal is to find the
+                best solution for any given problem. I am constantly learning new technologies and methods
+                for improving my workflow.
+              </p>
+              <br/>
+              <AnchorLink href={skills} id="check-out">
+                CHECK ME OUT
+              </AnchorLink>
             </section>
         )
     }
